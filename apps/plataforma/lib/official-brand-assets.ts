afa002f9f56797efcf124548923fa9f508a0cca8
Pedro Guilherme Lemos Teixeira <pglemos31@gmail.com>
2026-08-04 02:33:16 -0300

    brand: centralize official UDK assets

---
+++ apps/plataforma/lib/official-brand-assets.ts
@@ -0,0 +1,5 @@
+export const officialBrandAssets = {
+  wordmarkWhite: "/brand/udk-wordmark-white.svg",
+  wordmarkDark: "/brand/udk-wordmark-dark.svg",
+  avatar: "/icons/udk-avatar-512.png",
+} as const;
